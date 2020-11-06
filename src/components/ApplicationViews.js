0a11ca3e7440188cc71d6969775fb2b1e023e1a9
justha <nganha42@gmail.com>
2020-11-06 10:28:06 -0600

    registration, login and providers for games and events

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,5 +1,10 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { GameProvider } from "./game/GameProvider"
+import { GameList } from "./game/GameList"
+import { EventProvider } from "./event/EventProvider"    
+import { EventList } from "./event/EventList"    
+
 
 export const ApplicationViews = () => {
     return <>
@@ -9,5 +14,18 @@ export const ApplicationViews = () => {
         }}>
             Application views
         </main>
+
+        <GameProvider>
+            <Route exact path="/">
+                <GameList />
+            </Route>
+        </GameProvider>
+
+        <EventProvider>
+            <Route exact path="/events">
+                <EventList />
+            </Route>
+        </EventProvider>
+
     </>
 }
